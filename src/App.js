83a3b4e150326cef56b065af1319acc9e97d47b8
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-07-05 19:21:46 +0530

    updates

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from "./components/Form";
+import Recipes from "./components/Recipes"
+
+// const YOUR_APP_ID = "69526d8e";
+// const YOUR_APP_KEY = "f07c0bf2f3c232fe2445a4c354afb337";
+
+
+class App extends Component {
+
+  state = {
+    count: 0,
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+
+    // const api_call = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
+    const api_call = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${recipeName}`);
+
+    // console.log(recipeName);
+    const data = await api_call.json();
+    // console.log(data);
+    this.setState({
+      count: data.count,
+      recipes: data.recipes
+    });
+    console.log(this.state.count)
+    console.log(this.state.recipes)
+  }
+  render() {
+    const {
+      getRecipe,
+      state: {
+        count,
+        recipes
+      }
+    } = this;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={getRecipe} />
+        <Recipes recipes={recipes} count={count} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Recipes.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Recipes(props) {
+    return (
+        <div className="container">
+            <div className="row">
+                {/* {props.count} */}
+                {props.recipes.map((recipe) => {
+                    return (
+                        <div key={recipe.recipe_id} className="col-md-4" style={{ marginBottom: "2rem" }}>
+                            <div className="recipes__box">
+                                <img
+                                    className="recipe__box-img"
+                                    src={recipe.image_url}
+                                    alt={recipe.title} />
+                                <div className="recipe__text">
+                                    <h5 className="recipes__title">
+                                        {recipe.title.length < 20 ? `${recipe.title}` : `${recipe.title.substring(0, 25)}...`}
+                                    </h5>
+                                    <p className="recipes__subtitle">Publisher:
+                                        <span>{recipe.publisher}</span>
+                                    </p>
+                                </div>
+                                <button className="recipe_buttons">View Recipe</button>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    )
+}
